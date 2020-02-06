89c9989521be78a69687d78dd769049fb436a988
Anthony Kuske <anthonykuske@gmail.com>
2020-02-06 16:54:51 +0000

    Receipt getting there

--- src/prepData.js
+++ src/prepData.js
@@ -11,15 +11,24 @@ const isMerchant = (receiptType) => !!receiptType.match(/^Merchant/i);
 const isCustomer = (receiptType) => !!receiptType.match(/^Customer/i);
 
 /**
+ * Verifone sends the expiry date as MMYY but on their example receipts has it has MM/YY.
+ * Change it to that format.
+ *
+ * @param {string} expiry
+ * @return {string}
+ */
+const formatExpiryDate = (expiry) => expiry.slice(0, 2) + '/' + expiry.slice(2);
+
+/**
  * Accepts an object of data and adds a bunch of extra properties to help with rendering it.
  *
  * @param {object} data
- *
  * @return {object}
  */
 const prepData = (data) => {
     data.isMerchant = isMerchant(data.ReceiptType);
     data.isCustomer = isCustomer(data.ReceiptType);
+    data.formattedExpiryDate = data.ExpiryDate ? formatExpiryDate(data.ExpiryDate) : '';
 
     return data;
 };
